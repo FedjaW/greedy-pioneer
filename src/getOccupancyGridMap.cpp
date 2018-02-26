75353c68c49809c947ff3cf3476bb7248ccb26c2
FedjaW <F.Wilms89@web.de>
2018-02-26 15:28:03 +0100

    set marker where i want

--- src/getOccupancyGridMap.cpp
+++ src/getOccupancyGridMap.cpp
@@ -3,10 +3,15 @@
 #include <vector>
 #include <fstream>
 #include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
 
 using namespace std;
 
+    // void setMarker(visualization_msgs::Marker& marker, ros::NodeHandle &nh);
+    void setMarker(ros::NodeHandle &nh, int mark_x, int mark_y);
+    // visualization_msgs::Marker marker_global;
 
+bool waitForSubscribers(ros::Publisher & pub, ros::Duration timeout);
 // grid map
 int rows;
 int cols;
@@ -17,26 +22,92 @@ double map_frame_x;
 double map_frame_y;
 
 bool requestMap(ros::NodeHandle &nh);
-void readMap(const nav_msgs::OccupancyGrid& msg);
+void readMap(const nav_msgs::OccupancyGrid& msg, ros::NodeHandle &nh);
 void printGridToFile();
 void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg);
+
 
 int main (int argc, char** argv){
 
     ros::init(argc, argv, "getOccupancyGridMap"); 
     ros::NodeHandle nh;
-
     ros::Subscriber sub = nh.subscribe("odometry/filtered",10,OdomCallback);
     ros::Rate rate(10);
     rate.sleep();
     ros::spinOnce();
 
+
     if (!requestMap(nh))
         exit(-1);
 
-
-    printGridToFile();
+    // printGridToFile();
     return 0;
+}
+
+
+void setMarker(ros::NodeHandle &nh, int mark_x, int  mark_y){
+            ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 20);
+    visualization_msgs::Marker marker;
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "/map";
+    marker.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = "basic_shapes";
+    marker.id = 0;
+
+    // Set the marker type.  
+    marker.type = visualization_msgs::Marker::CUBE;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = visualization_msgs::Marker::ADD;
+
+    marker.pose.position.x = mark_x;
+    marker.pose.position.y = mark_y;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = 0.2;
+    marker.scale.y = 0.2;
+    marker.scale.z = 0.2;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 1.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+
+    ros::Duration timeout = ros::Duration(3);
+    if(waitForSubscribers(marker_pub, timeout)){
+        marker_pub.publish(marker);
+        ROS_INFO("habe marker gesetzt");
+    }
+    else
+        ROS_INFO("TIMEOUT");
+}
+
+
+
+
+bool waitForSubscribers(ros::Publisher & pub, ros::Duration timeout)
+{
+    if(pub.getNumSubscribers() > 0)
+        return true;
+    ros::Time start = ros::Time::now();
+    ros::Rate waitTime(0.5);
+    while(ros::Time::now() - start < timeout) {
+        waitTime.sleep();
+        if(pub.getNumSubscribers() > 0)
+            break;
+    }
+    return pub.getNumSubscribers() > 0;
 }
 
 
@@ -54,7 +125,12 @@ bool requestMap(ros::NodeHandle &nh){
   ros::ServiceClient mapClient = nh.serviceClient<nav_msgs::GetMap>("dynamic_map");
 
   if(mapClient.call(req, res)){
-    readMap(res.map);
+    readMap(res.map, nh);
+
+                 
+                 // ROS_INFO("res map orin = %.3f\n",res.map.info.origin.position.x);
+
+
     return true;
   }
   else{
@@ -64,7 +140,7 @@ bool requestMap(ros::NodeHandle &nh){
 }
 
 
-void readMap(const nav_msgs::OccupancyGrid& map){
+void readMap(const nav_msgs::OccupancyGrid& map, ros::NodeHandle &nh){
     ROS_INFO("Received a %d X %d Map @ %.3f m/px\n", 
                                                     map.info.width,
                                                     map.info.height,
@@ -80,6 +156,11 @@ void readMap(const nav_msgs::OccupancyGrid& map){
     int grid_frame_y = (unsigned int) ((map_frame_y - map.info.origin.position.y)/map.info.resolution);
   ROS_INFO("grid_frame_x = %d \n", grid_frame_x);
   ROS_INFO("grid_frame_y = %d \n", grid_frame_y);
+
+                 int mark_pos_x = map_frame_x;
+                 int mark_pos_y = map_frame_y;
+                 setMarker(nh, mark_pos_x, mark_pos_y);
+
 
   rows = map.info.height;
   cols = map.info.width;
