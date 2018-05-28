079b2eeb6630c091221fbbed7fc06a26c361fa55
FedjaW <F.Wilms89@web.de>
2018-05-28 12:05:00 +0200

    robot_yaw wird jetzt mit tf::getYaw ausgelese

--- src/move_service.h
+++ src/move_service.h
@@ -3,11 +3,17 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Point.h>
-
 #include "myGetMap.h"
+
+// extern tf::StampedTransform robotPosInMapFrame;
+
 // void getDistanceToFrontier(ros::NodeHandle &nh, geometry_msgs::Point goalCanditate);
 double getDistanceToFrontier(ros::NodeHandle &nh, geometry_msgs::Point goalCanditate);
 double getDistance(double x1,double y1,double x2,double y2);
 
 tf::StampedTransform getRobotPosInMapFrame();
+
+
+
+
 #endif
--- src/myMain.cpp
+++ src/myMain.cpp
@@ -18,6 +18,7 @@ int main (int argc, char **argv) {
     std::thread watcherThread(startPositionWatcher);
     ROS_INFO("Testpunnkt 2");
 
+     ros::Rate rate2(0.5);
      ros::Rate rate(10);
      rate.sleep(); // warte kurz bis die Callbacks im anderen thread
      rate.sleep(); // aufgerufen wurden und Daten vorliegen
@@ -27,58 +28,36 @@ int main (int argc, char **argv) {
 
     nav_msgs::OccupancyGrid grid = requestMap(nh);
     std::vector<std::vector<int> > gridMap = readMap(grid);
-
-    // ROS_INFO("gridMap[5][5] = %d", gridMap[5][5]);
-	// ROS_INFO("gridMap.size() = %d", gridMap.size());
-    // std::vector<std::vector<int> > costMap = getCostmap();
-	// ROS_INFO("costMap.size() = %d", costMap.size());
-    //geometry_msgs::Point myPoint = myMap.grid2Kartesisch(grid,0,0);
-
-    // ROS_INFO("Punkt x = %f", myPoint.x);
-    // ROS_INFO("Punkt y = %f", myPoint.y);
-    
-    //gridCell myCell =  myMap.kartesisch2grid(grid,-10,-10);
-
-    // ROS_INFO("cell row = %d", myCell.row);
-    // ROS_INFO("cell col = %d", myCell.col);
     
 
     std::vector<geometry_msgs::Pose> myVizPos;
     geometry_msgs::Pose dummyPos;
 
 
-    ROS_INFO("x: %f / y: %f", getRobotPos(/*nh*/).x, getRobotPos(/*nh*/).y);
+    ROS_INFO("x: %f / y: %f", getRobotPos().x, getRobotPos().y);
     //ROS_INFO("myRobot.yaw = %f", myRobot.yaw);
 
     geometry_msgs::Point myPoint;
 
-    // for(int i = costMap.size() - 1; i >= 0 ; i--){      // durchsuche ganze Karte
-    //     for(int j = 0; j < costMap[0].size(); j++){
-    //         if(costMap[i][j] == 10){
-    //             //ROS_INFO("255");
-    //             myPoint = grid2Kartesisch(grid,j,i);
-    //             dummyPos.position.x = myPoint.x;
-    //             dummyPos.position.y = myPoint.y;
-    //             myVizPos.push_back(dummyPos);
-    //         }
-    //
-    //     }
-    // }
-    
-    // double x_ = getRobotPosInMapFrame().getOrigin().x();
-    // double y_ = getRobotPosInMapFrame().getOrigin().y();
-
-    //dummyPos.position.x = myRobot.x;
-    //dummyPos.position.y = myRobot.y;
-
     std::vector<gridCell> frontierCells = findFrontierCells(gridMap);
-// für die Funktion fillFrontier / alle variablem sind Global
-    double robotPos_x = getRobotPosInMapFrame().getOrigin().x();
-    double robotPos_y = getRobotPosInMapFrame().getOrigin().y();
+    // für die Funktion fillFrontier / alle variablem sind Global
+    double robotPos_x = getRobotPosInMapFrame().getOrigin().x(); //getRobotPosInMapFrame erzeugt ein globales Objekt
+    double robotPos_y = getRobotPosInMapFrame().getOrigin().y(); // vom TransformListener
     robotPos_col = kartesisch2grid(grid, robotPos_x, robotPos_y).col;
     robotPos_row = kartesisch2grid(grid, robotPos_x, robotPos_y).row;
-    robot_yaw = atan2(robotPos_y, robotPos_x);
-// _______________________________________________________________________-
+    robot_yaw = tf::getYaw(getRobotPosInMapFrame().getRotation());
+    std::cout << "robot_yaw = " << robot_yaw << std::endl;
+    rate2.sleep();
+    rate2.sleep();
+    rate2.sleep();
+    robot_yaw = tf::getYaw(getRobotPosInMapFrame().getRotation());
+    std::cout << "robot_yaw = " << robot_yaw << std::endl;
+    rate2.sleep();
+    rate2.sleep();
+    rate2.sleep();
+    robot_yaw = tf::getYaw(getRobotPosInMapFrame().getRotation());
+    std::cout << "robot_yaw = " << robot_yaw << std::endl;
+    // _______________________________________________________________________
     std::vector<Frontier> frontier_list = buildFrontiers(frontierCells);
     
     for(auto& frontier : frontier_list) {
